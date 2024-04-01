3860a93edc8d2b2828dbc3f5f146265d5bf035d4
kyu08 <49891479+kyu08@users.noreply.github.com>
2024-04-01 16:18:37 +0900

    lecture4-memory

---
+++ lecture4-memory/address.c
@@ -0,0 +1,9 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main() {
+  char *s = "HI!";
+  printf("%c\n", s[0]);
+  printf("%c\n", s[1]);
+  printf("%c\n", s[2]);
+}
---
+++ lecture4-memory/notAllocated.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  int scores[3];
+  for (int i = 0; i < 3; i++) {
+    printf("%i\n", scores[i]);
+  }
+}
